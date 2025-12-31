b8fe258989572b1e82f64c00877a20550847cc9c
Abdullah <abdullah262001@gmail.com>
2025-12-31 14:17:20 +0530

    feat(routing): add routing and removed some xtra files made for testing layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Edit from "./pages/Edit/Edit";
+import Upload from "./pages/Upload/Upload";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* MAIN APP LAYOUT */}
+        <Route path="/" element={<Edit />}>
+          {/* Home → just layout, no doc */}
+          <Route index element={null} />
+
+          {/* Edit document */}
+          <Route path="edit/:id" element={null} />
+        </Route>
+
+        {/* Upload page */}
+        <Route path="/upload" element={<Upload />} />
+
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
